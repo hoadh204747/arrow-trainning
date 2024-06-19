8a570b44eac79eaa237d7b85041e6109bc8b6062
Đỗ Huy Hòa <dohuyhoa12012001gmail.com>
2024-06-19 10:47:03 +0700

    attribute binding from component AtributeBinding

---
+++ vuejs/vue-project/src/components/AttributeBiding.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Cristiano Ronaldo</h1>
+        <img :src="imageUrl" alt="Cristiano Ronaldo">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                imageUrl:"https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
