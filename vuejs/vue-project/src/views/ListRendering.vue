3c7472a296124a8eb76c9b378e66dc017e66703c
Đỗ Huy Hòa <dohuyhoa12012001gmail.com>
2024-06-26 00:35:54 +0700

    computed, watch

--- vuejs/vue-project/src/views/ListRendering.vue
+++ vuejs/vue-project/src/views/ListRendering.vue
@@ -1,33 +1,53 @@
 <script setup>
-    import {ref} from 'vue'
+    import {computed, ref, watch} from 'vue'
     import TodoItem from '../components/TodoItem.vue'
 
+    const feedbackText=ref('')
     const newTodo = ref('')
     let todoId = 0
     const todos = ref([
 
     ])
 
+    watch(newTodo,  (value) => {
+        if(value === ''){
+            feedbackText.value = ''
+        } else if(!value.includes('@'))  {
+            feedbackText.value = 'Please enter a valid email'
+        } else {
+            feedbackText.value = 'okkkkkk'
+        }
+    })
+
     function addNewTodo() {
+        if (newTodo.value.includes('@')) {
         todos.value.push({
             id: todoId,
             name: newTodo.value
-        }),
-        newTodo.value = '';
-        todoId++;
+        })
+        newTodo.value = ''
+        todoId++
+        feedbackText.value = ''
+        }
     }
+    const computedTodo = computed(() => {
+        return todos.value.length > 0 ? `You have ${todos.value.length} email` : 'No email'
+    })
+
+
 </script>
 
 <template>
     <form v-on:submit.prevent="addNewTodo">
-        <label for="new-todo">Add a todo</label>
+        <label for="new-todo">Add email</label>
         <input
             v-model="newTodo"
             id="new-todo"
-            placeholder="new todo ..."
+            placeholder="email..."
         >
         <button>Add</button>
     </form>
+    
     <ul>
         <TodoItem
             v-for="(todo, index) in todos"
@@ -37,6 +57,8 @@
             @remove="todos.splice(index, 1)"
         ></TodoItem>
     </ul>
+    <span>{{ feedbackText }}</span>
+    <span>{{ computedTodo }}</span>
 </template>
 
 <style lang="scss" scoped>
